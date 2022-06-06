a3c7a689fb600fccaf64439c82418899800a84a3
gideonlangat98 <gideonlangat98@gmail.com>
2022-06-06 12:09:20 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solutions in this file
+
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+//     debugger;
+//   }
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     debugger;
+//   }
+
+//   return gifts;
+// }
+
+// wrapGifts(gifts);
+
+const cards = (["Guadalupe", "Ollie", "Aki"], "birthday");
+
+function writeCards(name, event) {
+    let thankYouMessage = [];
+    for(let i = 0; i < name.length; i++) {
+        thankYouMessage.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+
+}
+return thankYouMessage;
+
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "birthday");
+
+//Count down
+function countDown(i){
+while (i > 0){
+    console.log(i);
+    i--;
+}
+console.log(i);
+
+}
